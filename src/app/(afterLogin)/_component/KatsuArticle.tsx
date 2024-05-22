c057052c4df576da0ef4dd6148100b71bc06aba7
heisjun <jun146517@gmail.com>
2024-05-22 15:25:51 +0900

    Feat : 돈까스정보 상세페이지 이동 컴포넌트 추가
    돈까스 정보를 서버컴포넌트로 사용하기 위해 컴포넌트 분리

---
+++ src/app/(afterLogin)/_component/KatsuArticle.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import { ReactNode } from "react";
+import styles from "@/app/(afterLogin)/_component/katsuInfo.module.css";
+import { useRouter } from "next/navigation";
+import { IKatsuInfo } from "@/model/KatsuInfo";
+
+type Props = {
+  children: ReactNode;
+  post: IKatsuInfo;
+};
+
+export default function KatsuArticle({ children, post }: Props) {
+  const router = useRouter();
+  let target = post;
+
+  const onClick = () => {
+    router.push(`/${target.name}`);
+  };
+
+  return (
+    <article onClick={onClick} className={styles.infoWrapper}>
+      {children}
+    </article>
+  );
+}
