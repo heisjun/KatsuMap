f0a9b06046f8339849a33d276d534fb993b984e6
heisjun <jun146517@gmail.com>
2024-05-24 17:33:28 +0900

    Feat : 가게 상세정보페이지 추가

--- src/app/(afterLogin)/[storename]/page.tsx
+++ src/app/(afterLogin)/[storename]/page.tsx
@@ -1,5 +1,30 @@
 import styles from "@/app/(afterLogin)/[storename]/storeInfo.module.css";
+import KatsuDetail from "./_component/katsuDetail";
+import {
+  HydrationBoundary,
+  QueryClient,
+  dehydrate,
+} from "@tanstack/react-query";
+import { getStoreInfo } from "../_lib/getStoreInfo";
+import { getSingleKatsuInfo } from "./_lib/getSingleKatsuInfo";
 
-export default function StoreInfo() {
-  return <div>가게상세정보</div>;
+interface Props {
+  params: { storename: string };
+}
+
+export default async function StoreInfo({ params }: Props) {
+  const { storename } = params;
+  const queryClient = new QueryClient();
+  await queryClient.prefetchQuery({
+    queryKey: ["store", storename],
+    queryFn: getSingleKatsuInfo,
+  });
+  const dehydratedState = dehydrate(queryClient);
+  return (
+    <>
+      <HydrationBoundary state={dehydratedState}>
+        <KatsuDetail storename={storename} />
+      </HydrationBoundary>
+    </>
+  );
 }
