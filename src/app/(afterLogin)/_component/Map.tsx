8e61c9ac0f97659274c6c9510c6ed4f8b784268b
heisjun <jun146517@gmail.com>
2024-05-27 22:07:21 +0900

    Feat : 카카오맵 인포윈도우 추가
    지도상에 보여질 인포윈도우 추가&디자인

--- src/app/(afterLogin)/_component/Map.tsx
+++ src/app/(afterLogin)/_component/Map.tsx
@@ -1,26 +1,14 @@
 "use client";
-
+import styles from "@/app/(afterLogin)/_component/map.module.css";
 import { IKatsuInfo } from "@/model/KatsuInfo";
-import {
-  MapMarker,
-  useKakaoLoader as useKakaoLoaderOrigin,
-} from "react-kakao-maps-sdk";
+import { CustomOverlayMap, MapMarker } from "react-kakao-maps-sdk";
 import { Map } from "react-kakao-maps-sdk";
-
-function useKakaoLoader() {
-  useKakaoLoaderOrigin({
-    appkey: process.env.KAKAOJSKEY as string,
-    libraries: ["clusterer", "drawing", "services"],
-  });
-}
 
 type Props = {
   store: IKatsuInfo;
 };
 
 export default function BasicMap({ store }: Props) {
-  useKakaoLoader();
-
   return (
     <Map // 지도를 표시할 Container
       id="map"
@@ -42,20 +30,20 @@ export default function BasicMap({ store }: Props) {
           lat: Number(store.location.lat),
           lng: Number(store.location.lng),
         }}
-      />
-      <div style={{ padding: "5px", color: "#000" }}>
-        {store.name} <br />
-        <a
-          href={`https://map.kakao.com/link/to/${store.name},${Number(
-            store.location.lat
-          )},${Number(store.location.lng)}`}
-          style={{ color: "blue" }}
-          target="_blank"
-          rel="noreferrer"
-        >
-          길찾기
-        </a>
-      </div>
+      ></MapMarker>
+      <CustomOverlayMap // 커스텀 오버레이를 표시할 Container
+        // 커스텀 오버레이가 표시될 위치입니다
+        position={{
+          lat: Number(store.location.lat),
+          lng: Number(store.location.lng),
+        }}
+        yAnchor={0}
+      >
+        {/* 커스텀 오버레이에 표시할 내용입니다 */}
+        <div className={styles.infoWrapper}>
+          <span className={styles.storeName}>{store.name}</span>
+        </div>
+      </CustomOverlayMap>
     </Map>
   );
 }
