fea0218d987e0d2a238585eb6d014bde9f257c0f
heisjun <jun146517@gmail.com>
2024-06-02 16:34:40 +0900

    Style : API 주소 수정
    API Route Handler 이름에 맞게 수정

--- src/app/(afterLogin)/_lib/getStoreInfo.ts
+++ src/app/(afterLogin)/_lib/getStoreInfo.ts
@@ -1,10 +1,13 @@
 export async function getStoreInfo() {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/katsuList`, {
-    next: {
-      tags: ["store", "info"],
-    },
-    cache: "no-store",
-  });
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_BASE_URL}/api/getStoreList`,
+    {
+      next: {
+        tags: ["store", "info"],
+      },
+      cache: "no-store",
+    }
+  );
   if (!res) {
     throw new Error("Failed to fetch data");
   }
