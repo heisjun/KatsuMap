91b74233b3c74a9e6e713b54c1a60449045a541d
heisjun <jun146517@gmail.com>
2024-06-07 23:51:14 +0900

    Feat : ImgSlider 컴포넌트 추가

--- src/app/(afterLogin)/[postId]/_component/katsuDetail.tsx
+++ src/app/(afterLogin)/[postId]/_component/katsuDetail.tsx
@@ -1,9 +1,11 @@
 "use client";
 
-import { useQuery, useQueryClient } from "@tanstack/react-query";
+import { useQuery } from "@tanstack/react-query";
 import { IKatsuInfo } from "@/model/KatsuInfo";
 import { getSingleKatsuInfo } from "../_lib/getSingleKatsuInfo";
 import BasicMap from "../../_component/Map";
+import styles from "./katsuDetail.module.css";
+import ImgSlider from "./ImgSlider";
 
 interface Props {
   postId: string;
@@ -27,14 +29,51 @@ export default function KatsuDetail({ postId }: Props) {
   if (!store) {
     return null;
   }
+  const testData = [
+    { picUrl: store.image_url },
+    { picUrl: store.image_url },
+    { picUrl: store.image_url },
+  ];
 
   return (
-    <div>
-      <div>{store.name}</div>
-      <img src={store.image_url} />
-      <div>{store.explain}</div>
+    <div className={styles.detailContainer}>
+      <div className={styles.detailTitle}>{store.title}</div>
+      <ImgSlider data={testData} />
+      <div className={styles.infoContainer}>
+        <div className={styles.infoBlockLeft}>
+          <div className={styles.storeName}>{store.name}</div>
+          {store.explain.split("<br>").map((content, idx) => (
+            <div key={idx}>
+              <span className={styles.storeExplain}>{content}</span>
+              <br />
+              <br />
+            </div>
+          ))}
+        </div>
+        <div className={styles.infoBlockRight}>
+          <div className={styles.infoAdress}>주소</div>
+          <div className={styles.addressContent}>{store.address}</div>
+          <div className={styles.infoAdress}>메뉴</div>
+          <div className={styles.flexContainer}>
+            {store.menu
+              .split("/")
+              .map((menuInfo) => menuInfo.split(" "))
+              .map((ele, idx) => (
+                <div className={styles.category} key={idx}>
+                  <div className={styles.menuName}>{ele[0]}</div>
+                  <div className={styles.menuPrice}>{ele[1]}</div>
+                </div>
+              ))}
+          </div>
+          <div className={styles.infoAdress}> 영업시간</div>
+          {store.time.split("/").map((info, idx) => (
+            <div key={idx}>
+              <div className={styles.addressContent}>{info}</div>
+            </div>
+          ))}
+        </div>
+      </div>
       <BasicMap store={store} />
-      <div>웨이팅 확인</div>
     </div>
   );
 }
