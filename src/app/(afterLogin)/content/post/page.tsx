bb5c2833ce9ad9d794e1e652eb22cf21530a0d8c
heisjun <jun146517@gmail.com>
2024-06-20 17:46:49 +0900

    Feat : 로그아웃 버튼 컴포넌트 추가

--- src/app/(afterLogin)/content/post/page.tsx
+++ src/app/(afterLogin)/content/post/page.tsx
@@ -1,8 +1,13 @@
 import styles from "./post.module.css";
+import LogoutBtn from "../../_component/LogoutBtn";
+import { auth } from "@/auth";
+
 export default async function WritePage() {
+  const session = await auth();
   return (
     <div className={styles.postContainer}>
       <div className={styles.contentContainer}></div>
+      <LogoutBtn me={session} />
     </div>
   );
 }
