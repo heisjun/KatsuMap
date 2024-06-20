d621246eb196324a4c944828973746f1a6d1c467
heisjun <jun146517@gmail.com>
2024-06-20 17:45:29 +0900

    Feat : useMutation을 이용한 스크랩 기능 추가
    useMutation을 이용하여 스크랩기능을 구현하였고, onMutate()를 이용하여 스크랩 클릭시 즉각적인 변화를 확인할 수 있도록 구현

--- src/app/(afterLogin)/_component/KatsuInfo.tsx
+++ src/app/(afterLogin)/_component/KatsuInfo.tsx
@@ -1,16 +1,149 @@
+"use client";
+
 import styles from "@/app/(afterLogin)/_component/katsuInfo.module.css";
 import { IKatsuInfo } from "@/model/KatsuInfo";
 import KatsuArticle from "./KatsuArticle";
+import { BsBookmark, BsBookmarkFill } from "react-icons/bs";
+import { v4 as uuidv4 } from "uuid";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { MouseEventHandler } from "react";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
 
 type Props = {
   info: IKatsuInfo;
 };
+
 export default function KatsuInfo({ info }: Props) {
+  const session = useSession();
+  const router = useRouter();
+  const queryClient = useQueryClient();
   let target = info;
+
+  const scrapId = uuidv4();
+  const user_email = session.data?.user?.email;
+  const postId = info.post_id;
+  const scrap = useMutation({
+    mutationFn: async () => {
+      return fetch(
+        `${process.env.NEXT_PUBLIC_BASE_URL}/api/posts/${postId}/scrap`,
+        {
+          method: "POST",
+          credentials: "include",
+          body: JSON.stringify({ scrapId, user_email, postId }),
+        }
+      );
+    },
+    onMutate() {
+      //실시간 변화
+      const queryCache = queryClient.getQueryCache();
+      const queryKeys = queryCache.getAll().map((cache) => cache.queryKey);
+      queryKeys.forEach((queryKey) => {
+        if (queryKey[0] === "store") {
+          const value: IKatsuInfo | IKatsuInfo[] | undefined =
+            queryClient.getQueryData(queryKey);
+          if (Array.isArray(value)) {
+            const index = value.findIndex((v) => v.post_id === postId);
+            //인덱스가 있다면
+            if (index > -1) {
+              const shallow = [...value];
+              shallow[index] = {
+                ...shallow[index],
+                is_scrap: 1,
+              };
+              queryClient.setQueryData(queryKey, shallow);
+            }
+          } else if (value) {
+            //상세보기인 경우
+            if (value.post_id === postId) {
+              const shallow = {
+                ...value,
+                is_scrap: 1,
+              };
+              queryClient.setQueryData(queryKey, shallow);
+            }
+          }
+        }
+      });
+    },
+    onError() {
+      alert("오류가 발생했습니다!");
+    },
+    onSettled() {},
+  });
+
+  const unscrap = useMutation({
+    mutationFn: async (unScrapId: string) => {
+      return fetch(
+        `${process.env.NEXT_PUBLIC_BASE_URL}/api/posts/${postId}/scrap`,
+        {
+          method: "delete",
+          credentials: "include",
+          body: JSON.stringify({ unScrapId }),
+        }
+      );
+    },
+    onMutate(unScrapId: string) {
+      //실시간 변화
+      const queryCache = queryClient.getQueryCache();
+      const queryKeys = queryCache.getAll().map((cache) => cache.queryKey);
+      queryKeys.forEach((queryKey) => {
+        if (queryKey[0] === "store") {
+          const value: IKatsuInfo | IKatsuInfo[] | undefined =
+            queryClient.getQueryData(queryKey);
+          if (Array.isArray(value)) {
+            const index = value.findIndex((v) => v.post_id === postId);
+            //인덱스가 있다면
+            if (index > -1) {
+              const shallow = [...value];
+              shallow[index] = {
+                ...shallow[index],
+                is_scrap: 0,
+              };
+              queryClient.setQueryData(queryKey, shallow);
+            }
+          } else if (value) {
+            //상세보기인 경우
+            if (value.post_id === postId) {
+              const shallow = {
+                ...value,
+                is_scrap: 0,
+              };
+              queryClient.setQueryData(queryKey, shallow);
+            }
+          }
+        }
+      });
+    },
+    onError() {
+      alert("오류가 발생했습니다!");
+    },
+    onSettled() {},
+  });
+
+  const onClickScrap: MouseEventHandler<HTMLDivElement> = (e) => {
+    e.stopPropagation();
+    if (session.data === null) {
+      router.push("/login");
+    } else {
+      if (info.is_scrap === 1) {
+        unscrap.mutate(info.scrap_id);
+      } else {
+        scrap.mutate();
+      }
+    }
+  };
   return (
     <KatsuArticle post={target}>
-      <img src={info.image_url} className={styles.storeImage} />
+      <div style={{ position: "relative" }}>
+        <img src={info.image_url} className={styles.storeImage} />
+        <div className={styles.storeBookMark} onClick={onClickScrap}>
+          {info.is_scrap === 1 ? <BsBookmarkFill /> : <BsBookmark />}
+        </div>
+      </div>
+
       <div className={styles.storeTitle}>{info.title}</div>
+
       <div className={styles.storeName}>{info.name}</div>
       <div className={styles.storeLocation}>{info.address}</div>
     </KatsuArticle>
