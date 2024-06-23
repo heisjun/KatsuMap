a041ed09847f75871d8670cf81ef3fb6264f23eb
heisjun <jun146517@gmail.com>
2024-06-23 18:28:05 +0900

    Feat : 로그인 모달 생성
    패러렐라우트와 인터셉트라우트를 활용하여 모달형식의 로그인폼 생성

--- src/app/(afterLogin)/layout.tsx
+++ src/app/(afterLogin)/layout.tsx
@@ -4,12 +4,13 @@ import RQProvider from "./_component/RQProvider";
 import NavMenu from "./_component/NavMenu";
 import { auth } from "@/auth";
 
-type Props = { children: ReactNode };
-export default async function AfterLoginLayout({ children }: Props) {
+type Props = { children: ReactNode; modal: ReactNode };
+export default async function AfterLoginLayout({ children, modal }: Props) {
   const session = await auth();
   return (
     <div className={style.container}>
       <RQProvider>
+        {modal}
         <NavMenu session={session} />
         <main className={style.main}>{children}</main>
       </RQProvider>
