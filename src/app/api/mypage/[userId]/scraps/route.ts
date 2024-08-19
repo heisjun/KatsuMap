46c7461cb18b2053d7f97e685334a5f3bda3d09d
heisjun <jun146517@gmail.com>
2024-08-19 18:32:07 +0900

    Chore : 주석 삭제

--- src/app/api/mypage/[userId]/scraps/route.ts
+++ src/app/api/mypage/[userId]/scraps/route.ts
@@ -9,20 +9,12 @@ export async function GET(
 ) {
   noStore();
   try {
-    // Artificially delay a response for demo purposes.
-    // Don't do this in production :)
-
-    // console.log('Fetching revenue data...');
-    // await new Promise((resolve) => setTimeout(resolve, 3000));
-
     const data =
       await sql<KatsuInfo>`SELECT s.post_id, s.name, s.title, s.explain, s.image_url, s.lat, s.lng, s.address, s.time, s.menu,
       us.scrap_id, 
       CASE WHEN us.scrap_id IS NOT NULL THEN 1 ELSE 0 END AS is_scrap
 			FROM katsu_info s
 			JOIN scraps us ON s.post_id = us.post_id and us.user_email =${params.userId}`;
-
-    // console.log('Data fetch completed after 3 seconds.');
 
     return NextResponse.json(data.rows);
   } catch (error) {
--- src/app/api/store/[postId]/detail/route.ts
+++ src/app/api/store/[postId]/detail/route.ts
@@ -9,16 +9,8 @@ export async function GET(
 ) {
   noStore();
   try {
-    // Artificially delay a response for demo purposes.
-    // Don't do this in production :)
-
-    // console.log('Fetching revenue data...');
-    // await new Promise((resolve) => setTimeout(resolve, 3000));
-
     const data =
       await sql<KatsuInfo>`SELECT * FROM katsu_info where post_id = ${params.postId}`;
-
-    // console.log('Data fetch completed after 3 seconds.');
 
     return NextResponse.json(data.rows[0]);
   } catch (error) {
