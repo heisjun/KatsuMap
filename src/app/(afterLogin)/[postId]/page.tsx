f15917b295afff9b15b7c544224e78934e90dad0
heisjun <jun146517@gmail.com>
2024-08-30 18:44:19 +0900

    Feat : meta 데이터 추가
    소셜 미디어 플랫폼에서 링크 미리보기에 필요한 openGraph 속성 추가

--- src/app/(afterLogin)/[postId]/page.tsx
+++ src/app/(afterLogin)/[postId]/page.tsx
@@ -20,12 +20,25 @@ export async function generateMetadata({ params }: Props) {
     title: `${store.name} / KatsuMap`,
     description: `${store.title}`,
     icons: {
-      icon: `${store.image_url}`,
+      icon: "/logoIcon.png",
+    },
+    openGraph: {
+      title: `${store.name} / KatsuMap`,
+      description: `${store.title} 상세보기`,
+      url: `${process.env.NEXT_PUBLIC_BASE_URL}/${params.postId}`,
+      images: [
+        {
+          url: `${store.image_url}`, // 썸네일 이미지 URL
+          alt: `${store.name} Thumbnail`, // 이미지 대체 텍스트
+        },
+      ],
+      siteName: "KatsuMap", // 사이트 이름
     },
   };
 }
 
 export default async function StoreInfo({ params }: Props) {
+  console.log("링크:", `${process.env.NEXT_PUBLIC_BASE_URL}/${params.postId}`);
   const { postId } = params;
   const queryClient = new QueryClient();
   await queryClient.prefetchQuery({
