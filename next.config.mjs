1178bef69f20ca318f033982ac581cf68046e715
heisjun <jun146517@gmail.com>
2024-09-02 21:54:34 +0900

    Feat : Next/Image 에서 aws s3이미지 사용시 필요한 도메인 추가

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,9 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
+  images: {
+    domains: ["d36dcj71lapw9d.cloudfront.net"], // 여기에 외부 도메인 추가
+  },
 };
 
 export default nextConfig;
