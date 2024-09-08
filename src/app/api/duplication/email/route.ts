80a2b946dc639b2e14381c3e6528dc42780c87bb
heisjun <jun146517@gmail.com>
2024-09-08 16:50:57 +0900

    Feat : 회원가입 관련 API 추가
    회원가입 & 이메일 중복체크 API 생성

---
+++ src/app/api/duplication/email/route.ts
@@ -0,0 +1,38 @@
+import { NextRequest, NextResponse } from "next/server";
+import { sql } from "@vercel/postgres";
+import { User } from "@/app/_lib/definitions";
+
+async function getUser(email: string): Promise<User | undefined> {
+  try {
+    const user = await sql<User>`SELECT * FROM users WHERE email=${email}`;
+    return user.rows[0];
+  } catch (error) {
+    console.error("Failed to fetch user:", error);
+    throw new Error("Failed to fetch user.");
+  }
+}
+
+function convertProvider(provider: string | null) {
+  if (provider === null) {
+    return "katsumap으";
+  } else if (provider === "kakao") {
+    return "카카오 간편가입으";
+  } else if (provider === "naver") {
+    return "네이버 간편가입으";
+  }
+}
+
+// POST 함수 정의
+export async function POST(request: NextRequest) {
+  const { email } = await request.json();
+  const user = await getUser(email);
+
+  // 이미 가입되어 있는 계정이면 오류 메시지를 응답으로 전달
+  if (user) {
+    // 사용자가 존재하면 provider 정보를 반환
+    return NextResponse.json(convertProvider(user.provider));
+  } else {
+    // 사용자가 존재하지 않으면 null 반환
+    return NextResponse.json(null);
+  }
+}
--- src/app/api/register/route.ts
+++ src/app/api/register/route.ts
@@ -24,13 +24,15 @@ async function getUser(email: string): Promise<User | undefined> {
 async function createUser(
   id: string,
   name: string,
+  nickname: string,
   email: string,
-  password: string
+  password: string,
+  image: string
 ): Promise<User> {
   try {
     const result: QueryResult<User> = await sql`
-      INSERT INTO users (id, name, email, password)
-      VALUES (${id}, ${name}, ${email}, ${password})
+      INSERT INTO users (id, name, email, password, image, nickname)
+      VALUES (${id}, ${name}, ${email}, ${password},${image},${nickname})
       RETURNING *;
     `;
     return result.rows[0];
@@ -43,33 +45,35 @@ async function createUser(
 // POST 함수 정의
 export async function POST(request: NextRequest) {
   // 1. 회원가입에 입력한 정보 받아오기
-  const body = await request.json();
+  const { name, nickname, email, password, image } = await request.json();
 
   // 2. 데이터 유효성 검사
-  const validation = schema.safeParse(body);
+  const validation = schema.safeParse({ email, password });
   // 오류가 발생하면 오류 메시지를 응답으로 리턴
   if (!validation.success) {
     return NextResponse.json(validation.error.errors, { status: 400 });
   }
 
   // 3. 가입 여부 검증
-  const user = await getUser(body.email);
+  const user = await getUser(email);
   // 이미 가입되어 있는 계정이면 오류 메시지를 응답으로 전달
   if (user) {
     return NextResponse.json({ error: "User already exists" }, { status: 400 });
   }
 
   // 4. 비밀 번호 암호화
-  const hashedPassword = await bcrypt.hash(body.password, 10);
+  const hashedPassword = await bcrypt.hash(password, 10);
 
   // 5. 사용자 생성
   const userId = uuidv4();
   try {
     const newUser = await createUser(
       userId,
-      body.name,
-      body.email,
-      hashedPassword
+      name,
+      nickname,
+      email,
+      hashedPassword,
+      image
     );
     // 6. 생성된 사용자의 정보를 응답으로 반환
     return NextResponse.json({ newUser }, { status: 201 });
